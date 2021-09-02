3e2f802f5670d45f19c69769fff3b2dfa3704b4b
Ashutosh Tripathi <ashuuu38@gmail.com>
2021-09-02 18:22:09 +0530

    req

--- qr_generate/settings.py
+++ qr_generate/settings.py
@@ -124,8 +124,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static_cdn"),)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 
